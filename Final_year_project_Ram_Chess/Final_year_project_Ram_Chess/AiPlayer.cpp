3bb3e0b651e5cc06ea5e1f9bf9300566f010c87a
Ram Pawar <ram_pawar2000@hotmail.com>
2019-05-07 22:05:51 +0100

    Depth comment

--- Final_year_project_Ram_Chess/Final_year_project_Ram_Chess/AiPlayer.cpp
+++ Final_year_project_Ram_Chess/Final_year_project_Ram_Chess/AiPlayer.cpp
@@ -3,6 +3,7 @@
 
 Movement AiPlayer::DoMovement(Chessboard& chessboard)
 {
+	//Bigger the depth, slower the speed is. 
 	//Recommended depth is in range 1-5.
 	return Ai::FindBestMovement(chessboard, 3, !chessboard.is_black());
 }
